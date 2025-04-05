2175c29feaf9af348b1b98ebabffe6c2799f969c
bnogimauemura <bnogimauemurak@icloud.com>
2025-04-05 23:52:42 +0900

    2665. Counter II - ACCEPTED Runtime: 34ms

---
+++ 30-Days-of-JS/CounterII.js
@@ -0,0 +1,23 @@
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+// The three functions are:
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
+
+const createCounter = function(init) {
+    let startValue = init;
+    return {
+        increment: function() {
+            startValue += 1
+            return startValue;
+        },
+        decrement: function() {
+            startValue -= 1;
+            return startValue;
+        },
+        reset: function() {
+            startValue = init;
+            return startValue
+        }
+};
+}
